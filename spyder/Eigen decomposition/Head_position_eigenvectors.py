2a5fb44612bf9c1082ab119b31dda9b5a473f9b9
Anna Shishkina <shishki.ann@gmail.com>
2019-04-17 20:22:11 +0300

    Eigenvectors in 5 positions for 4 coils

--- spyder/Eigen decomposition/Head_position_eigenvectors.py
+++ spyder/Eigen decomposition/Head_position_eigenvectors.py
@@ -7,7 +7,7 @@ Created on Fri Apr 12 17:33:02 2019
 
 import mne
 import numpy as np
-from scipy.linalg import eigh 
+from scipy.linalg import eig 
 import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
 
@@ -19,7 +19,7 @@ import matplotlib.ticker as ticker
 def coil_position (fname):
     raw = mne.io.read_raw_fif(fname, preload=True)
     raw.info['bads'] += ['MEG1433','MEG1822','MEG1843','MEG1412','MEG0943','MEG1033']
-    raw.info['sfreq'] = 700.
+    raw.info['sfreq'] = 648.
     picks = mne.pick_types(raw.info, meg='grad', eeg=False, eog=False,
                        stim=False, exclude='bads')
     raw.crop(0.,120.)
@@ -27,19 +27,22 @@ def coil_position (fname):
     for l_freq, h_freq in zip([290, 305.5, 313, 319.5], [294, 308.5, 315, 322.5]):
         raw_copy = raw.copy()
         frange = raw_copy.filter(l_freq=l_freq, h_freq=h_freq, picks=picks)
-        ch_time = frange.get_data(picks=picks)
+        ch_time = frange.get_data(picks=picks, start = 0, stop = 120001)
         matrix = ch_time.dot(ch_time.T)
-        w, v = eigh(matrix, eigvals=(197,197))
-        vectors.append(v)
+        w, v = eig(matrix)
+        princ = v[:1]
+        vectors.append(princ)
     position = np.hstack(vectors)
+    position.reshape
     return np.array(position)
-    
+  
+a = front.reshape(4,198)
 
-base = coil_position('head_movement\\190131\\base_opened_raw.fif')
-front = coil_position('head_movement\\190131\\front_opened_raw.fif')
-back = coil_position('head_movement\\190131\\back_opened_raw.fif')
-left = coil_position('head_movement\\190131\\left_opened_raw.fif')
-right = coil_position('head_movement\\190131\\right_opened_raw.fif')
+base = coil_position('head_movement\\190131\\base_opened_raw.fif').reshape(4,198).transpose()
+front = coil_position('head_movement\\190131\\front_opened_raw.fif').reshape(4,198).transpose()
+back = coil_position('head_movement\\190131\\back_opened_raw.fif').reshape(4,198).transpose()
+left = coil_position('head_movement\\190131\\left_opened_raw.fif').reshape(4,198).transpose()
+right = coil_position('head_movement\\190131\\right_opened_raw.fif').reshape(4,198).transpose()
 
 
 #------------------------------------------------------------------------------
@@ -75,5 +78,20 @@ plt.savefig('head_pos_eig.png')
 
 
 
+raw = mne.io.read_raw_fif('head_movement\\190131\\base_opened_raw.fif', preload=True)
+raw.info['bads'] += ['MEG1433','MEG1822','MEG1843','MEG1412','MEG0943','MEG1033']
+raw.info['sfreq'] = 700.
+picks = mne.pick_types(raw.info, meg='grad', eeg=False, eog=False,
+                       stim=False, exclude='bads')
+raw.crop(0.,120.)
+frange = raw.filter(l_freq=290, h_freq=294, picks=picks)
+ch_time = frange.get_data(picks=picks)
+matrix = ch_time.dot(ch_time.T)
+w, v = eig(matrix)
+
+print()
 
 
+vectors.append(princ)
+    position = np.hstack(vectors)
+    return np.array(position)
